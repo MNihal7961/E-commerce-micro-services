1548e6a2fb6ceb94e7c4b6ab426231fe681261ba
M Nihal <mnihal@Ms-MacBook-Air.local>
2024-05-23 15:32:21 +0530

    initialised and completed the server configuration auth service

---
+++ auth-service/infrastructure/database/dbConnection.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose"
+import dotenv from 'dotenv'
+dotenv.config()
+
+
+export default async () => {
+    try {
+
+        const mongoUrl = process.env.MONGO_URL
+
+        if (!mongoUrl) {
+            throw new Error("MongoDB connection string not provided in environment variables ❌")
+        }
+
+        await mongoose.connect(mongoUrl.trim())
+
+    } catch (error: Error | any) {
+        console.error(`❌❌❌ Database Connection failed ❌❌❌`)
+        console.error(error.message)
+        process.exit(1)
+    }
+}
---
+++ auth-service/presentation/server.ts
@@ -0,0 +1,33 @@
+import express, { Application, NextFunction, Request, Response } from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+import cookieParser from 'cookie-parser'
+
+
+const app: Application = express()
+const PORT: number = Number(process.env.PORT) || 80001
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+    console.log(error.message)
+
+    const errorResponse = {
+        errors: [{
+            message: error?.message || "Something went wrong ❌"
+        }]
+    }
+
+    return res
+        .status(500)
+        .json(errorResponse)
+})
+
+app.listen(PORT, () => {
+    console.log("Auth Service Running ✅")
+    console.log(`http://localhost:${PORT}`)
+})
+
+export default app
