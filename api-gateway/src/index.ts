bd9facaa2159f5387750ff7c611919167bcc4444
M Nihal <mnihal@Ms-MacBook-Air.local>
2024-05-24 11:01:08 +0530

    added api gateway and tested ✅

---
+++ api-gateway/src/index.ts
@@ -0,0 +1,23 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import proxy from 'express-http-proxy'
+
+
+const app = express()
+const PORT = process.env.PORT || 9000
+
+app.use(cors())
+app.use(express.json())
+app.use(cookieParser())
+
+const authProxy = "http://localhost:8001/"
+
+app.use('/auth', proxy(authProxy))
+
+app.listen(PORT, () => {
+    console.log("API Gateway Running ✅ ")
+    console.log(`http://localhost:${PORT}`)
+})
