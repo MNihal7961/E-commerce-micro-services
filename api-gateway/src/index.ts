4b79ce922635ea797bcaa30cab3d9435ef55f646
M Nihal <mnihal@Ms-MacBook-Air.local>
2024-05-24 19:49:25 +0530

    added product proxy

--- api-gateway/src/index.ts
+++ api-gateway/src/index.ts
@@ -15,9 +15,11 @@ app.use(cookieParser())
 
 const authProxy = "http://localhost:8001/"
 const adminProxy = "http://localhost:8002/"
+const productProxy = "http://localhost:8003"
 
 app.use('/auth', proxy(authProxy))
 app.use('/admin', proxy(adminProxy))
+app.use('/product', proxy(productProxy))
 
 app.listen(PORT, () => {
     console.log("API Gateway Running ✅ ")
